a32e71f004448520ee82cd7e11c381e5f2de19de
bfdev <bfdev@kakao.com>
2020-03-21 17:22:18 +0000

    ACL 함수 외부로 옮김

--- route/edit_document.js
+++ route/edit_document.js
@@ -3,17 +3,7 @@ const markdown = require("../util/markdown")
 const getUser = require("../util/getUser")
 const getACL = require("../util/getACL")
 const getDocument = require("../util/getDocument")
-async function checkPermission(req,type){
-	//type: String 
-	if(getUser.getName(req)){
-		var UserACL = (await getUser.getUser(getUser.getName(req))).ACL //유저의 ACL 구하기
-	}else{
-		var UserACL = ["any"] //비로그인 유저의 경우 any 권한만 부여
-	}
-	const DocumentACL = (await getACL.getDocumentACL(req.params.document))[type] //문서의 ACL 구하기
-	const Match = DocumentACL.filter(x => UserACL.includes(x)).length != 0
-	return Match //일치하는 권한이 없을 때 false, 있을때는 true
-}
+
 module.exports = function (app){
 	app.get("/edit/:document", async (req, res) => {
 		const GetDocument = await Document.findOne({Title: req.params.document})
@@ -30,15 +20,15 @@ module.exports = function (app){
         <div id="preview">
         </div>
 		`
-		if(!await checkPermission(req,"Read")){var edit_content = '<span style="color:red;position:relative;bottom:12px;">읽을 권한이 없어 편집할수 없습니다.</span><br></br>'}
+		if(!await getACL.checkPermission(req,"Read")){var edit_content = '<span style="color:red;position:relative;bottom:12px;">읽을 권한이 없어 편집할수 없습니다.</span><br></br>'}
 		const data = {data:edit_content,title:`${req.params.document}`,username:getUser.getName(req)}
 		res.render("index.html",data)
 	})
 	app.post("/edit/:document", async (req, res) => {
 		const GetDocument = await Document.findOne({Title: req.params.document})
 		const DocumentStatus = (GetDocument == null) 
-		if(!await checkPermission(req,"Read")){return res.redirect(`/edit/${req.params.document}?err=읽을 권한이 없습니다.`)}
-		if(!await checkPermission(req,"Edit")){return res.redirect(`/edit/${req.params.document}?err=편집 권한이 없습니다.`)}
+		if(!await getACL.checkPermission(req,"Read")){return res.redirect(`/edit/${req.params.document}?err=읽을 권한이 없습니다.`)}
+		if(!await getACL.checkPermission(req,"Edit")){return res.redirect(`/edit/${req.params.document}?err=편집 권한이 없습니다.`)}
 		if(req.body.content == ""){return res.redirect(`/edit/${req.params.document}?err=문서 내용이 없습니다.`)}
 		if(!DocumentStatus){if(req.body.content == GetDocument.Data){return res.redirect(`/edit/${req.params.document}?err=문서 내용이 같습니다.`)}}
 		await getDocument.editDocument(req.params.document, req.body.content, getUser.getName(req), req.body.cause)
--- util/getACL.js
+++ util/getACL.js
@@ -1,4 +1,5 @@
 const Document = require("./schema/document")
+const getUser = require("../util/getUser")
 exports.getDocumentACL = async function (documentName){
     const before = await Document.findOne({Title: documentName})
     const check = (before == null)
@@ -36,9 +37,9 @@ exports.getDocumentACL = async function (documentName){
     }else if(namespace == "사용자"){
         return {
             Read:((result.ACL_Read.length == 0) ? ["any"]:result.ACL_Read),
-            Edit:((result.ACL_Edit.length == 0) ? [`user:${result.split(":")[1]}`,"admin"]:result.ACL_Edit),
-            Remove:((result.ACL_Remove.length == 0) ? [`user:${result.split(":")[1]}`,"admin"]:result.ACL_Remove),
-            Move:((result.ACL_Move.length == 0) ? [`user:${result.split(":")[1]}`,"admin"]:result.ACL_Move),
+            Edit:((result.ACL_Edit.length == 0) ? [`user:${documentName.split(":")[1]}`,"admin"]:result.ACL_Edit),
+            Remove:((result.ACL_Remove.length == 0) ? [`user:${documentName.split(":")[1]}`,"admin"]:result.ACL_Remove),
+            Move:((result.ACL_Move.length == 0) ? [`user:${documentName.split(":")[1]}`,"admin"]:result.ACL_Move),
             Discuss:((result.ACL_Discuss.length == 0) ? ["any"]:result.ACL_Discuss),
             EditRequest:((result.ACL_EditRequest.length == 0) ? ["member"]:result.ACL_EditRequest),
         }
@@ -71,4 +72,16 @@ exports.getDocumentACL = async function (documentName){
         }
         
     }
+}
+exports.checkPermission = async function (req,type){
+    //type: String 
+    
+	if(getUser.getName(req)){
+		var UserACL = (await getUser.getUser(getUser.getName(req))).ACL //유저의 ACL 구하기
+	}else{
+		var UserACL = ["any"] //비로그인 유저의 경우 any 권한만 부여
+	}
+	const DocumentACL = (await exports.getDocumentACL(req.params.document))[type] //문서의 ACL 구하기
+	const Match = DocumentACL.filter(x => UserACL.includes(x)).length != 0
+	return Match //일치하는 권한이 없을 때 false, 있을때는 true
 }
